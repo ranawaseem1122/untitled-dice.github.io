48e3f6acd163df1545c80ff6607b33a3456d971d
ranawaseem1122 <ranawaseemsajid@outlook.com>
2016-06-05 03:14:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: ranawaseem1122,                             // <----------------------------- EDIT ME!
+  app_id: 1243,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'BitsNBank Dice!',
   // - For your faucet to work, you must register your site at Recaptcha
