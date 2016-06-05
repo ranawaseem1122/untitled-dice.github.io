e184f475b7ca91991a552de79e3990ccf8ca78c9
ranawaseem1122 <ranawaseemsajid@outlook.com>
2016-06-05 03:21:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfJzyETAAAAAArRsI_s2TyBUwqIPZk76LCTmQz7',  // <----- EDIT ME!
-  redirect_uri: 'https://bitsnbank.github.io',
+  redirect_uri: 'https://bitsnbankdice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
